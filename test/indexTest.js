8bb76f67eb5b485db618df9e40d8831018de0c80
github username <github email address>
2019-02-25 17:10:00 -0500

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,23 @@
-const expect = chai.expect;
+const chai = require('chai')
+global.expect = chai.expect
+const fs = require('file-system')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
+const babel = require('babel-core');
+
+const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
+
+const babelResult = babel.transformFileSync(
+ path.resolve(__dirname, '..', 'index.js'), {
+   presets: ['env']
+ }
+);
+
+const src = babelResult.code
+
+jsdom({
+ html, src
+});
 
 describe('index.js', function () {
   describe('titleCased()', function () {
@@ -7,7 +26,7 @@ describe('index.js', function () {
     });
 
     it('returns an array with title case tutorial names', function () {
-      expect(titleCased).to.have.all.members(
+      expect(titleCased(tutorials)).to.have.all.members(
         [
           "What Does The This Keyword Mean?",
           "What Is The Contutorialuctor OO Pattern?",
