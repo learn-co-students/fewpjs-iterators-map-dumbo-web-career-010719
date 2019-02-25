d797d7a5cacdd94bb773601422ea9002927e3160
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-02-25 17:30:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+const tutorials = [
+    'what does the this keyword mean?',
+    'What is the Contutorialuctor OO pattern?',
+    'implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    'What is NaN and how Can we Check for it',
+    'What is the difference between stopPropagation and preventDefault?',
+    'Immutable State and Pure Functions',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
+  ];
+
+const titleCased = (tutorials) =>{
+    const newArray=tutorials.map(word => word.split(" "))
+    const result=newArray.map(sent => sent.map(str=> str.charAt(0).toUpperCase()+ str.slice(1)).join(" "))
+    return result;
+}
